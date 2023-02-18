55ff353cc62f949e5fff842bb8487c18525561b8
JohnOmoha <omoha.john@yahoo.com>
2023-02-18 14:32:03 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prins the alphetic
+ * Return: Always (success)
+ */
+/* more headers goes there */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
