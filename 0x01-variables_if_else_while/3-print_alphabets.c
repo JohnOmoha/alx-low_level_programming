2e7e8451b6bf49392302507f8d41ee8b2b114d62
JohnOmoha <omoha.john@yahoo.com>
2023-02-18 15:23:00 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,18 +10,18 @@ int main(void)
 {
 	char c;
 	char d;
+
 	c = 'a';
 	d = 'A';
+
 	while
-		(c <= 'z') {
+		(c <= 'z')
 			putchar(c);
-			c++
-		}
-	while
-		(d <= 'z') {
+			c++;
+			while
+		(d <= 'z')
 			putchar(d);
-			d++
-		}
-	putchar('\n');
+			d++;
+			putchar('\n');
 	return (0);
 }
