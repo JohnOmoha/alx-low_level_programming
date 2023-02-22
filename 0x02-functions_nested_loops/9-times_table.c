4a505024993dda09e2319b3be7456dcd53d08ded
JohnOmoha <omoha.john@yahoo.com>
2023-02-22 12:45:54 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,6 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+
 /**
  * times_table -  a function that prints the 9 times table, starting with 0
  * rone = row, cone = column, d = digits of current result
