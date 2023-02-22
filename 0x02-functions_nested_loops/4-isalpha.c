6173db27bf7aabaad23e82618180b112c759989f
JohnOmoha <omoha.john@yahoo.com>
2023-02-21 19:25:34 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - check for char alphabet
  * @c: the char to be checked
- * Return: 1 if char is letter lower or upper, 0 otherwise
+ * Return: 1 if char is letter lowercase or uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
