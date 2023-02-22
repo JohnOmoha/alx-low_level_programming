e237a5040e635556b07b5844818d3d0cbc17ceae
JohnOmoha <omoha.john@yahoo.com>
2023-02-22 11:22:01 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,18 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: the number to be treated
+ * @r: the number to be treated
  * Return: value of the last digit of number
  */
 
-int print_last_digit(int)
+int print_last_digit(int r)
 {
 	int last;
-	
+
+	if (r < 0)
+		last = -1 * (r % 10);
+	else
+		last = r % 10;
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
