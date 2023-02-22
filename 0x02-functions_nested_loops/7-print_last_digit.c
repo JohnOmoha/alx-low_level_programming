171b42743ac273b12fb67d16e9ee7582a6e6b4a2
JohnOmoha <omoha.john@yahoo.com>
2023-02-22 08:51:51 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,12 @@
+#include "main.h"
 
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to be treated
+ * Return: value of the last digit of number
+ */
+
+int print_last_digit(int)
+{
+	int last;
+	
